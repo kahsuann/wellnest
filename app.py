3323321beffa0d8cc33d8c65cac6a124c7e8af13
kahsuann <kahsuan@hotmail.sg>
2025-02-13 17:48:43 +0000

    Initialise users and cases

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,10 +10,10 @@ db = SQLAlchemy(app)
 
 class Clinician(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(10), nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text, unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    role = db.Column(db.Text, nullable=False)
 
     def __init__(self, name, username, password, role='Junior'):
         self.name = name
@@ -23,7 +23,7 @@ class Clinician(db.Model):
 
 class Case(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.Text, nullable=False)
     description = db.Column(db.Text, nullable=False)
 
     def __init__(self, name, description):
@@ -33,10 +33,33 @@ class Case(db.Model):
 @app.before_request
 def create_tables():
     db.create_all()
-
-@app.route('/')
-def home():
-    return render_template("index.html")
+    # register clinicians
+    if not Clinician.query.first():
+        db.session.add_all([
+            Clinician(name='Joel Tan',
+                    username='joel87',
+                    password='pwjoel87',
+                    role='Senior'),
+            Clinician(name='Huang Shimin',
+                    username='shiminh',
+                    password='pwshiminh'),
+            Clinician(name='Rishi Agarwal',
+                    username='rishiaw',
+                    password='pwrishiaw')
+        ])
+    
+    # register cases
+    if not Case.query.first():
+        db.session.add_all([
+            Case(name='Jonathan Lim',
+                description='A 28-year-old software engineer who is experiencing intense anxiety during team meetings and is struggling to speak up, fearing judgment from colleagues.'),
+            Case(name='Angela Paolo',
+                 description='A 42-year-old teacher who is coping with the recent loss of a parent and is finding it difficult to concentrate on work and daily responsibilities.'),
+            Case(name='Xu Yaoming',
+                 description='A 16-year-old high school student who is feeling overwhelmed by academic pressure and is struggling to balance schoolwork, extracurriculars, and personal time.')
+        ])
+    
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run()
